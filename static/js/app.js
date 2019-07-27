fdaebf75c8db154970904e117db8e69e70e2ba7d
Blake Schwartzbach <blake2323@gmail.com>
2019-07-26 23:23:08 -0700

    complete filter button 7261123pm

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,54 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-
-//Append table to webpage
+// Append table to webpage
 var myTableBody = d3.select('tbody');
 
+// Define function that creates table based on input
 function populateTable(arrObj) {
 
-    
     for (var i = 0; i < arrObj.length; i++) {
 
         //add rows
         var myRow = myTableBody.append("tr");
 
-        // Loop 
+        // Add elements for each row;  
         for (var key in arrObj[i]) {
             var myTd = myRow.append('td');
             myTd.text(arrObj[i][key]);
         }
 
-
     }
 
 }
 
+// button that user clicks after entering "datetime"
 var filterButton = d3.select("#filter-btn");
 
-filterButton.on("click", function() {
-console.log("Hello world");
-// capture user input, create a "research .filter" (javascript function, filter array)
-// 
-//var filterData = tableData.filter()
 
-myTableBody.text("");
+// Parse and populate new table if user enters datetime and clicks button 'Filter Table'
+filterButton.on("click", function () {
 
-//populateTable(filterData)
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    // Display user input in console
+    console.log(inputValue);
+
+    // Define object such that all members ('each event') have datetime equivalent to user's input
+    var filteredData = tableData.filter(event => event.datetime === inputValue);
+
+    // Display updated table in console
+    console.log(filteredData);
+
+    //Clear table output on screen of webpage
+    myTableBody.text("");
+
+    // populate updated filteredData table 
+    populateTable(filteredData)
 });
 
 
-
+// populate all elements of table from data.js
 populateTable(tableData);
